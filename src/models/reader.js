2f1cf387f19b41af01621742c3c6c407df6fdc09
bluespringstest <71788357+bluespringstest@users.noreply.github.com>
2021-08-19 09:32:24 +0100

    added validation for readers

--- src/models/reader.js
+++ src/models/reader.js
@@ -1,9 +1,42 @@
 
 module.exports = (connection, DataTypes) => {
     const schema = {
-        name: DataTypes.STRING,
-        email: DataTypes.STRING,
-        password: DataTypes.STRING
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notEmpty: {
+                    args: [true],
+                    msg: 'Name cannot be empty',
+                },
+            }
+        },
+        email: {
+            type: DataTypes.STRING,
+            validate: {
+                notEmpty: {
+                args: [true],
+                msg: 'Email cannot be empty',
+            },
+            isEmail: {
+                args: [true],
+                msg: 'Please use a valid email address'
+            },
+        }
+        },
+        password: {
+            type: DataTypes.STRING,
+            validate: {
+                notEmpty: {
+                    args: [true],
+                    msg: 'Password cannot be empty'
+                },
+                isLessThan8Chars(value){
+                    if (value.length < 8)
+                    throw new Error('Password must be longer than 8 characters')
+                }
+            }
+        }
     };
     const ReaderModel = connection.define('Reader', schema);
     return ReaderModel;
